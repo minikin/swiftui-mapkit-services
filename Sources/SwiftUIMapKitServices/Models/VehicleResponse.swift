33ddb68b01a7942876ddc5d981c477b4fa383e81
minikin <djminikin@gmail.com>
2019-08-20 12:21:51 +0200

    Update VehicleResponse

--- Sources/SwiftUIMapKitServices/Models/VehicleResponse.swift
+++ Sources/SwiftUIMapKitServices/Models/VehicleResponse.swift
@@ -9,14 +9,39 @@
 import Foundation
 
 public struct VehicleResponse: Codable, Equatable {
-    public let id: Int
-    public let name: String
-    public let description: String
-    public let latitude: Double
-    public let longitude: Double
-    public let batteryLevel: Double
-    public let timestamp: String
-    public let price: Double
-    public let priceTime: Double
-    public let currency: String
+	// MARK: - Instance Properties
+	public let id: Int
+	public let name: String
+	public let description: String
+	public let latitude: Double
+	public let longitude: Double
+	public let batteryLevel: Double
+	public let timestamp: String
+	public let price: Double
+	public let priceTime: Double
+	public let currency: String
+	
+	// MARK: - Initialization
+	
+	public init(id: Int,
+							name: String,
+							description: String,
+							latitude: Double,
+							longitude: Double,
+							batteryLevel: Double,
+							timestamp: String,
+							price: Double,
+							priceTime: Double,
+							currency: String) {
+		self.id = id
+		self.name = name
+		self.description = description
+		self.latitude = latitude
+		self.longitude = longitude
+		self.batteryLevel = batteryLevel
+		self.timestamp = timestamp
+		self.price = price
+		self.priceTime = priceTime
+		self.currency = currency
+	}
 }

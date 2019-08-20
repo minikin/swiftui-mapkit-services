82404b476ec22ba3864f5759b9efba6bac3bc459
minikin <djminikin@gmail.com>
2019-08-20 10:08:13 +0200

    Add XCTestManifests

---
+++ Tests/SwiftUIMapKitServicesTests/XCTestManifests.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sasha Prokhorenko on 20.08.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+import XCTest
+
+#if !canImport(ObjectiveC)
+    public func allTests() -> [XCTestCaseEntry] {
+        return [
+        ]
+    }
+#endif
